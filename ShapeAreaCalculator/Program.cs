69246be057cd139930dac4688564b94ea9fee344
Daniel Page <dpage105@gmail.com>
2025-03-03 16:36:53 -0600

    Added method to calculate the area of a circle

---
+++ ShapeAreaCalculator/Program.cs
@@ -0,0 +1,14 @@
+﻿namespace ShapeAreaCalculator;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        
+    }
+    
+    public static double AreaOfCircle(double radius)
+    {
+        return Math.PI * (radius * radius);
+    }
+}
